9adba52b25d533f50ef6da4a3057c706daef014c
Eduardo de Souza Andrade <edu2andrade@gmail.com>
2023-04-18 13:08:39 +0200

    project uploaded

---
+++ src/js/component/home.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+const Home = () => {
+	const [selectedColor, setSelectedColor] = useState("red");
+	const [trafficLightColors, setTrafficLightColors] = useState({
+    red: "",
+    orange: "",
+    green: "",
+  });
+
+	useEffect(() => {
+		setTrafficLightColors({
+			red: 'selected',
+			orange: '',
+			green: '',
+		})
+	}, [])
+
+	const handleTrafficLightClick = (color) => {
+		setTrafficLightColors({
+      red: color === "red" ? "selected" : "",
+      orange: color === "orange" ? "selected" : "",
+      green: color === "green" ? "selected" : "",
+			// purple: color === "purple" ? "selected" : "",
+    })
+		
+		setSelectedColor(color)
+  };
+
+	const cycleColors = () => {
+    switch (selectedColor) {
+			case "red":
+				handleTrafficLightClick("orange")
+				break;
+			case "orange":
+				handleTrafficLightClick("green")
+				break;
+			case "green":
+				handleTrafficLightClick("purple")
+			break;
+		
+			default:
+				handleTrafficLightClick("red")
+				break;
+		}
+  };
+
+	const addPurpleColor = () => {
+    setTrafficLightColors({
+      ...trafficLightColors,
+      purple: "",
+    });
+  };
+
+	return (
+		<div className="mainContainer">
+			<div className="stick"></div>
+			<div className="trafficLightsContainer">
+				<div onClick={() => handleTrafficLightClick("red")} className={`light red ${trafficLightColors.red}`}></div>
+				<div onClick={() => handleTrafficLightClick("orange")} className={`light orange ${trafficLightColors.orange}`}></div>
+				<div onClick={() => handleTrafficLightClick("green")} className={`light green ${trafficLightColors.green}`}></div>
+				{(trafficLightColors.purple === "" || trafficLightColors.purple === "selected") && (
+          <div
+            onClick={() => handleTrafficLightClick("purple")}
+            className={`light purple ${trafficLightColors.purple}`}
+          ></div>
+        )}
+			</div>
+				<button className="button" onClick={cycleColors}>Cycle Lights</button>
+				<button className="button" onClick={addPurpleColor}>Add Purple Light</button>
+		</div>
+	);
+};
+
+export default Home;
